b82b8677dfd0fb032e3f52a27c1090601861f162
ymangel2 <ymangel2@node0.test.cs538-pg0.utah.cloudlab.us>
2025-10-05 19:52:41 -0600

    progress I think

--- scripts/generate_topology.sh
+++ scripts/generate_topology.sh
@@ -2,6 +2,7 @@
 
 CLIENTS=0
 CDNS=0
+LAB_NAME=test
 
 # parse args
 while [[ "$#" -gt 0 ]]; do
@@ -14,8 +15,12 @@ while [[ "$#" -gt 0 ]]; do
             CLIENTS="$2"
             shift 2
             ;;
+        --labname)
+            LAB_NAME="$2"
+            shift 2
+            ;;
         --help)
-            echo "Usage: $0 [--clients N] [--cdns N]"
+            echo "Usage: $0 [--clients N] [--cdns N] [--labname name]"
             exit 0
             ;;
         *)
@@ -25,13 +30,56 @@ while [[ "$#" -gt 0 ]]; do
     esac
 done
 
-echo "Generating topology with:"
+echo "Generating topology for lab \"$LAB_NAME\" with:"
 echo "- $CLIENTS Client node(s)"
 echo "- $CDNS CDN node(s)"
 
 # create directory structure in accordance with specified params:
 
-if [[ $CLIENTS -gt 0 ]]; then 
-	mkdir -p ~/client
+LAB_DIR="$HOME/$LAB_NAME"
+CONF_FILE="$LAB_DIR/lab.conf"
 
-exit 1
+mkdir -p "$LAB_DIR"
+touch "$CONF_FILE"
+
+if [[ $CDNS -gt 0 ]]; then 
+    mkdir -p "$LAB_DIR/cdn"
+
+    echo "# CDN Mesh (M):" > "$CONF_FILE"    
+    echo >> "$CONF_FILE"
+
+    for i in $(seq 1 "$CDNS"); do 
+        echo "cdn$i[0]=M" >> "$CONF_FILE"
+        echo "cdn$i[exec]=cdn" >> "$CONF_FILE"
+        # echo "cdn$i[image]=
+        if [[ $i -lt $CDNS ]]; then 
+            echo >> "$CONF_FILE"
+        fi
+    done
+fi
+
+if [[ $CLIENTS -gt 0 ]]; then 
+
+    echo >> "$CONF_FILE"
+
+	mkdir -p "$LAB_DIR/client"
+
+    echo "# Clients:" >> "$CONF_FILE"
+    echo >> "$CONF_FILE"
+
+    for i in $(seq 1 "$CLIENTS"); do 
+        echo "client$i[0]=C$i" >> "$CONF_FILE"
+        echo "client$i[exec]=client" >> "$CONF_FILE"
+        # echo "client$i[image]=
+        if [[ $CDNS -gt 0 ]]; then 
+            echo "cdn$(( RANDOM % CDNS + 1 ))[1]=C$i" >> "$CONF_FILE" # connect each client to random CDN 
+        fi
+        if [[ $i -lt $CLIENTS ]]; then 
+            echo >> "$CONF_FILE"
+        fi
+    done
+fi
+
+if [[ $CLIENTS -eq 0 && $CDNS -eq 0 ]]; then 
+    echo "You generated an empty topology. If this was not intended, please run \"$0 --help\"."
+fi
