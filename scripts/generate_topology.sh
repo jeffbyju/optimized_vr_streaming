1760b94926d109983730960662c17549a36c3069
ymangel2 <ymangel2@node0.test.cs538-pg0.utah.cloudlab.us>
2025-10-07 19:40:07 -0600

    yay

--- scripts/generate_topology.sh
+++ scripts/generate_topology.sh
@@ -42,6 +42,8 @@ CONF_FILE="$LAB_DIR/lab.conf"
 mkdir -p "$LAB_DIR"
 touch "$CONF_FILE"
 
+ip_idx=0
+
 if [[ $CDNS -gt 0 ]]; then 
 
     echo "# CDN Mesh (M):" > "$CONF_FILE"    
@@ -50,12 +52,15 @@ if [[ $CDNS -gt 0 ]]; then
     for i in $(seq 1 "$CDNS"); do 
         echo "cdn$i[0]=M" >> "$CONF_FILE"
         echo "cdn$i[exec]=cdn" >> "$CONF_FILE"
+        echo "cdn$i[num_terms]=0" >> "$CONF_FILE"
         # echo "cdn$i[image]=
         if [[ $i -lt $CDNS ]]; then 
             echo >> "$CONF_FILE"
         fi
 
         touch "$LAB_DIR/cdn$i.startup"
+        echo "ip addr add 10.0.0.$((ip_idx + 1))/24 dev eth0" >> "$LAB_DIR/cdn$i.startup"
+        ip_idx=$((ip_idx + 1))
     done
 fi
 
@@ -69,6 +74,7 @@ if [[ $CLIENTS -gt 0 ]]; then
     for i in $(seq 1 "$CLIENTS"); do 
         echo "client$i[0]=C$i" >> "$CONF_FILE"
         echo "client$i[exec]=client" >> "$CONF_FILE"
+        echo "client$i[num_terms]=0" >> "$CONF_FILE"
         # echo "client$i[image]=
         if [[ $CDNS -gt 0 ]]; then
             rand_idx=$((RANDOM % CDNS + 1))
@@ -77,6 +83,8 @@ if [[ $CLIENTS -gt 0 ]]; then
                 ((if_idx++))
             done
             echo "cdn$rand_idx[$if_idx]=C$i" >> "$CONF_FILE" # connect each client to random CDN 
+            echo "ip addr add 10.0.0.$((ip_idx + 1))/24 dev eth$if_idx" >> "$LAB_DIR/cdn$rand_idx.startup"
+            ip_idx=$((ip_idx + 1))
         fi
 
         if [[ $i -lt $CLIENTS ]]; then 
@@ -84,6 +92,9 @@ if [[ $CLIENTS -gt 0 ]]; then
         fi
 
         touch "$LAB_DIR/client$i.startup"
+
+        echo "ip addr add 10.0.0.$((ip_idx + 1))/24 dev eth0" >> "$LAB_DIR/client$i.startup"    
+        ip_idx=$((ip_idx + 1))
     done
 fi
 
